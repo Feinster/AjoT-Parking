b40bbc149ee8276ab05c14dd1a240fdfaf7e692a
Feinster <46670630+Feinster@users.noreply.github.com>
2023-07-06 18:57:05 +0200

    minor changes css + parking page

--- AjoT-Parking/src/app/app-routing.module.ts
+++ AjoT-Parking/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
+  {
+    path: 'parking',
+    loadChildren: () => import('./parking/parking.module').then( m => m.ParkingPageModule)
+  },
 ];
 
 @NgModule({
