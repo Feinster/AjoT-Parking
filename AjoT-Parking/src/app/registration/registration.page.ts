271c4538ac28b67096806a0d7d24c5e917bd2fcd
Feinster <46670630+Feinster@users.noreply.github.com>
2023-07-08 22:08:08 +0200

    form control added for email and password

--- AjoT-Parking/src/app/registration/registration.page.ts
+++ AjoT-Parking/src/app/registration/registration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  ionicForm: FormGroup;
 
-  ngOnInit() {
+  constructor(public formBuilder: FormBuilder) {
+    this.ionicForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  ngOnInit() { }
+
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  submitForm = () => {
+    if (this.ionicForm.valid) {
+      console.log(this.ionicForm.value);
+      return false;
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  };
 }
